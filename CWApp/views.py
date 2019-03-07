d7f8e9afd2cda0dc0e598eac456031c88b694b65
Joshtg1104 <joshtg1104@gmail.com>
2019-03-07 17:01:00 -0600

    Dang it

---
+++ CWApp/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+# index page endpoint
+def index(request):
+    return render(request, "CWApp/index.html")
+# not being used
+def index2(request):
+    return render(request, "CWApp/base.html")
+# about page endpoint
+def aboutUs(request):
+    return render(request, "CWApp/aboutUs.html")
+# gallery page endpoint
+def gallery(request):
+    return render(request, "CWApp/gallery.html")
+# contact page endpoint
+def contactUs(request):
+    return render(request, "CWApp/contactUs")
+# resource page endpoint
+def resources(request):
+    return render(request, "CWApp/resources.html")
+
+
